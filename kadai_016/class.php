c7d38b5c3b2c85f98f01ac1c6189bb2f3d731ecc
KShima0218 <165528320+KShima0218@users.noreply.github.com>
2024-08-13 01:11:18 +0900

    課題016提出用

---
+++ kadai_016/class.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+ <html lang="ja">
+ 
+ <head>
+     <meta charset="UTF-8">
+     <title>kadai_016_class.php</title>
+ </head>
+ 
+ <body>
+     <p>
+        <?php
+           class Food{
+                private $name;
+                private $price;
+
+                public function __construct(string $name , int $price){
+                    $this->name =$name;
+                    $this->price = $price;
+                }
+                public function Food_price(){
+                    echo $this->price. '<br>';
+                }
+
+            } 
+
+           class Animal{
+                private $name;
+                private $height;
+                private $weight;
+
+                public function __construct(string $name , int $height , int $weight){
+                    $this->name =$name;
+                    $this->height =$height;
+                    $this->weight =$weight;
+                }
+                public function Animal_height(){
+                    echo $this->height. '<br>';
+                }
+            
+            }
+
+           $Food = new Food('poteto' , 250);
+           print_r($Food);
+           echo '<br>';
+           $Animal = new Animal('dog' , 60 , 5000);
+           print_r($Animal); 
+           echo '<br>';
+           $Food->Food_price();
+           $Animal->Animal_height();
+           
+        ?>
+     </p>
+ </body>
+ 
+ </html>
